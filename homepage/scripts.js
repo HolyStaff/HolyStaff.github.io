fe5c5d2264db3c6e16dc6907843e6709671769f8
h9 <h9dead@gmail.com>
2024-03-04 20:58:20 +0100

    Fixed onclick event for item-1.

--- homepage/scripts.js
+++ homepage/scripts.js
@@ -1,3 +1,4 @@
 document.getElementById('item-1').addEventListener('click', () => {
-    alert('Do Not Touch You Absolute Idiot.')
+    alert('Do Not Touch You Absolute Idiot.') // Why does this work locally but not on github? lmao
+    // TODO - fix this
 })
